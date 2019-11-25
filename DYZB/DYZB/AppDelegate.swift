c22db6ca05fa8d7fcce33fe78b997db727b86a2d
AI-Esports-Wei <wangwei@zhijian-tech.com>
2019-11-25 19:48:29 +0800

    项目初始化

---
+++ DYZB/DYZB/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  DYZB
+//
+//  Created by 上海指坚网络科技有限公司 on 2019/11/25.
+//  Copyright © 2019 上海指坚网络科技有限公司. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+    // MARK: UISceneSession Lifecycle
+
+
+}
+
